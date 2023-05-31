aaf62039c94661de963c0658bc028f53845fbc2a
shreyam <shreyam91183@gmail.com>
2023-05-31 17:38:42 +0530

    change

--- exercise_3.js
+++ exercise_3.js
@@ -1,1 +1,3 @@
-// 
+                // tell me  a joke 
+// elem. innerHTML is used to populate a dist with HTML search online about this method and create a website with a dist tag can bring a random joke given an array of jokes. Use Math.random and fetch jokes from the internet (use any website to create the array).
+// Your website should show a random joke on every reload. Max length of your jokes array should be 10.
