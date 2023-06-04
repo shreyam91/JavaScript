c85e09f26ca9562db81cae45b1c4a0f6a4ad9ec3
shreyam <shreyam91183@gmail.com>
2023-06-04 09:46:50 +0530

    changes

---
+++ practice_8.js
@@ -0,0 +1,18 @@
+// write a program to show different alert when different buttons are clicked.
+
+//create a website which is capable of storig bookmarks of your favorite website using href.
+
+//repeat above questions using event listeners.
+
+//document.getElementById('google').addEventListener('click', function(){
+   // window.location = "https://google.com";
+   // window.focus();
+//})
+
+// write a js program to keep fetching contents of a website (every 5 seconds).
+
+// create a glowing bulb effect using classList toogle method in js.
+
+setInterval(async function(){
+    document.querySelector("#bulb").classList.toogle("bulb")
+}, 300)
