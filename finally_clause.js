57f2b1be3f5d6244eefb0cfd5050c40b6465e870
shreyam <shreyam91183@gmail.com>
2023-07-12 13:36:56 +0530

    new

---
+++ finally_clause.js
@@ -0,0 +1,22 @@
+/***************************************
+*                                      *
+*            finally clause            *
+*                                      *
+***************************************/
+
+try{
+    let a=0;
+    console.log(program)
+    console.log("Program run successfully");
+    return  // 
+}
+catch(err){
+    console.log("This is an error")
+}
+finally{
+    console.log("hey!this is finally")
+    //close the file
+    //exit the loop
+ }
+
+ //finally will run whether the try statement is executed fully or not.
